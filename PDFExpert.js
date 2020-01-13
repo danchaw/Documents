6aa70d9d49497d58e660abef51c642bae313a6b3
kiri.yang <danchaw@users.noreply.github.com>
2020-01-13 18:58:05 +0800

    PDFExpert.js

--- PDFExpert.js
+++ PDFExpert.js
@@ -1,11 +1,18 @@
-var obj = JSON.parse($response.body);
+/*
+^https:\/\/license\.pdfexpert\.com\/api\/.*\/(documents|pdfexpert6)\/subscription\/(refresh$|check$)
+*/
+var body = $response.body;
+var obj = JSON.parse(body);
+var obj = {
+  originalTransactionId: "520000468152521",
+  subscriptionState: "active",
+  isInGracePeriod: false,
+  subscriptionExpirationDate: "13:15 03/11/2099",
+  subscriptionAutoRenewStatus: "autoRenewOn",
+  isEligibleForIntroPeriod: false,
+  isPDFExpert6User: true,
+  isDocuments6User: true,
+  subscriptionReceiptId: "1572178404000"
+};
 
-obj["isInGracePeriod"] = false;
-obj["isEligibleForIntroPeriod"] = false;
-obj["originalTransactionId"] = "20000000000000";
-obj["subscriptionState"] = "active";
-obj["subscriptionExpirationDate"] = "23:59 31/12/2099";
-obj["subscriptionAutoRenewStatus"] = "autoRenewOff";
-obj["subscriptionReceiptId"] = "1500000000000";
-
-$done({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj)});
